646a0215d1cc355373097ec32015fb488fa15685
Rayhan Zulfitri Dwi Cahyo <rayhanzulfitri@gmail.com>
2024-09-13 16:51:07 +0700

    feat(seeder): Fix seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,3 @@
-import { kelasSeed } from "./seeds/kelas";
 import { materiSeed } from "./seeds/materi";
 import { nilaiSeed } from "./seeds/nilai";
 import { pelajaranSeed } from "./seeds/pelajaran";
@@ -7,13 +6,10 @@ import { prisma } from "./seeds/PrismaModule";
 import { roleSeed } from "./seeds/role";
 import { tugasSeed } from "./seeds/tugas";
 import { userSeed } from "./seeds/user";
-import { UserPadaKelasSeed } from "./seeds/UserPadaKelas";
 
 async function main() {
-    await kelasSeed()
     await roleSeed()
     await userSeed()
-    await UserPadaKelasSeed()
     await pelajaranSeed()
     await materiSeed()
     await tugasSeed()
